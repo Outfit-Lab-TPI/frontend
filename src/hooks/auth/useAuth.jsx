b57acee2ebab8d7b866f5f5f16ec36d0fcae3983
camilamarendazzo <camimarendazzo@gmail.com>
2025-11-11 23:14:44 -0300

    Arreglo auth context

--- src/hooks/auth/useAuth.jsx
+++ src/hooks/auth/useAuth.jsx
@@ -1,8 +1,8 @@
 import { useContext } from 'react'
-import { AuthProvider } from '../../context/AuthContext'
+import { AuthContext } from '../../context/AuthContext'
 
 export function useAuth() {
-  const context = useContext(AuthProvider)
+  const context = useContext(AuthContext)
   if (!context) {
     throw new Error('useAuth debe usarse dentro de AuthProvider')
   }
