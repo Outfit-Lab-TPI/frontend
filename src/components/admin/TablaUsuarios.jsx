c9e32995b932126f00a36de6491e3502f5940c11
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2025-11-20 23:51:55 -0300

    fix convert-to user & admin

--- src/components/admin/TablaUsuarios.jsx
+++ src/components/admin/TablaUsuarios.jsx
@@ -61,7 +61,6 @@ function TablaUsuarios({
                       : 'bg-secondary/20 text-secondary hover:bg-tertiary/30 hover:text-tertiary'
                   }`}
                 >
-                  <span>{usuario.role}--</span>
                   <span className="group-hover:hidden">
                     {usuario.role === 'ADMIN' ? 'Administrador' : 'Usuario'}
                   </span>
--- src/services/adminService.js
+++ src/services/adminService.js
@@ -93,7 +93,7 @@ export const adminService = {
   cambiarRolUsuario: async (userId, nuevoRol) => {
     try {
       // TODO: Implementar endpoint cuando esté disponible
-      let endpointToConvert = nuevoRol === 'administrador' ? 'convert-to-admin' : 'convert-to-user';
+      let endpointToConvert = nuevoRol === 'ADMIN' ? 'convert-to-admin' : 'convert-to-user';
       console.log(endpointToConvert + " -- nuevo rol->" + nuevoRol)
       return await apiClient.put(`/users/${endpointToConvert}/${userId}`);
 
